2ee94931f03f4d4a09ab694aa466000f1819def3
explore11 <prefect_start@163.com>
2020-12-20 16:59:59 +0800

    增加网关 并测试

---
+++ gulimall-gateway/src/test/java/com/song/gulimallgateway/GulimallGatemallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.song.gulimallgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallGatemallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
