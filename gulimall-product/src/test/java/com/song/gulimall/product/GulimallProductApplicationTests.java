637010357b87f1e58bec53e8e845eecab2f3e2f3
explore11 <prefect_start@163.com>
2021-01-23 22:20:10 +0800

    第三方服务

--- gulimall-product/src/test/java/com/song/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/song/gulimall/product/GulimallProductApplicationTests.java
@@ -4,6 +4,7 @@ import com.song.gulimall.product.entity.BrandEntity;
 import com.song.gulimall.product.service.BrandService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -14,7 +15,36 @@ import javax.annotation.Resource;
 public class GulimallProductApplicationTests {
     @Resource
     BrandService brandService;
- 
+
+    @Value("${oss.accessKeyId}")
+    private String accessKeyId;
+
+    @Value("${oss.accessKeySecret}")
+    private String accessKeySecret;
+
+    @Value("${oss.endpoint}")
+    private String endpoint;
+
+    @Value("${oss.bucket}")
+    private String bucket;
+
+//    @Test
+//    public void test() throws FileNotFoundException {
+//        // Endpoint以杭州为例，其它Region请按实际情况填写。
+//        // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
+//
+//        // 创建OSSClient实例。
+//        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+//
+//        // 上传文件流。
+//        InputStream inputStream = new FileInputStream("D:\\0d40c24b264aa511.jpg");
+//        ossClient.putObject(bucket, "99.jpg", inputStream);
+//        // 关闭OSSClient。
+//        ossClient.shutdown();
+//
+//        System.out.println("上传完成了.....");
+//    }
+
     @Test
     public void contextLoads() {
         BrandEntity brandEntity = new BrandEntity();
