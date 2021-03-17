d77e21a4e6a6ec00cd813b1a233e3a9e1752f110
explore11 <prefect_start@163.com>
2021-03-17 21:35:49 +0800

    增加搜索服务

---
+++ gulimall-search/src/main/java/com/song/gulimall/gulimallsearch/config/ElasticSearchConfig.java
@@ -0,0 +1,35 @@
+package com.song.gulimall.gulimallsearch.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.HttpAsyncResponseConsumerFactory;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/* *
+ * @program: gulimall
+ * @description
+ * @author: swq
+ * @create: 2021-03-17 21:17
+ **/
+@Configuration
+public class ElasticSearchConfig {
+
+    public static final RequestOptions COMMON_OPTIONS;
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+//        builder.addHeader("Authorization", "Bearer " + TOKEN);
+//        builder.setHttpAsyncResponseConsumerFactory(
+//                new HttpAsyncResponseConsumerFactory
+//                        .HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 * 1024));
+        COMMON_OPTIONS = builder.build();
+    }
+
+
+    @Bean
+    public RestHighLevelClient esClient() {
+        return new RestHighLevelClient(RestClient.builder(new HttpHost("192.168.56.10", 9200, "http")));
+    }
+}
---
+++ gulimall-search/src/test/java/com/song/gulimall/gulimallsearch/GulimallSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.song.gulimall.gulimallsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
