74d356c74f5b92f00f4bfc8e74778de634fc45d5
explore11 <prefect_start@163.com>
2021-03-27 00:04:31 +0800

    订单项目构建

--- gulimall-order/src/main/java/com/song/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/song/gulimall/order/GulimallOrderApplication.java
@@ -1,9 +1,17 @@
 package com.song.gulimall.order;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
+@EnableFeignClients("com.song.gulimall.order.feign")
+@EnableDiscoveryClient
+@EnableRabbit
 public class GulimallOrderApplication {
 
     public static void main(String[] args) {
---
+++ gulimall-order/src/main/java/com/song/gulimall/order/config/GulimallSessionConfig.java
@@ -0,0 +1,36 @@
+package com.song.gulimall.order.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+/* *
+ * session 配置
+ */
+@Configuration
+public class GulimallSessionConfig {
+
+    /* *
+     * 存入redis的序列化方式
+     * @return
+     */
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+        return new GenericJackson2JsonRedisSerializer();
+    }
+
+    /* *
+     * 设置客户端的暴露bean  解决不同的域名之间的访问  设置父域名
+     * @return
+     */
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setCookieName("GULISESSIONID");
+        serializer.setDomainName("gulimall.com");
+        return serializer;
+    }
+}
